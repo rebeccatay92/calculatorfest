75196dd862404e51fafb33c67038fee9d987d9d7
Rebecca Tay <rebeccatay92@gmail.com>
2017-06-30 11:38:16 +0800

    added delete event to clear the screen

--- js/main.js
+++ js/main.js
@@ -8,68 +8,71 @@ function init () {
   document.addEventListener('keyup', onKeyUp)
 
   function onKeyUp (event) {
-
+    console.log(event)
     if (checkKeyCode(event.keyCode)) {
       // update display with number corresponding to keycode
-      if (event.keyCode !== 13) { //if enter is not hit
-      display.value += ' ' +  event.key //add on to the input str
+      if (event.keyCode !== 13 && event.keyCode !== 8) { // if enter is not hit
+        display.value += ' ' + event.key // add on to the input str
       } else {
-      var displayValue = display.value
-      if (displayValue.indexOf('+') > -1) calculateAdd(displayValue)
-      if (displayValue.indexOf('-') > -1) calculateMinus(displayValue)
-      if (displayValue.indexOf('*') > -1) calculateMultiply(displayValue)
-      if (displayValue.indexOf('/') > -1) calculateDivision(displayValue)
+        var displayValue = display.value
+        if (displayValue.indexOf('+') > -1) calculateAdd(displayValue)
+        if (displayValue.indexOf('-') > -1) calculateMinus(displayValue)
+        if (displayValue.indexOf('*') > -1) calculateMultiply(displayValue)
+        if (displayValue.indexOf('/') > -1) calculateDivision(displayValue)
+        if (event.keyCode === 8) display.value = ''
       }
     }
-}
-  function calculateAdd(str) {
-    var splitStr = str.replace(/ /g, "").split('+')
+  }
+  function calculateAdd (str) {
+    var splitStr = str.replace(/ /g, '').split('+')
     console.log(splitStr)
     var firstNum = parseInt(splitStr[0])
     var secondNum = parseInt(splitStr[1])
-    var result = basic_calc.add(firstNum,secondNum)
+    var result = basic_calc.add(firstNum, secondNum)
     displayResult(result)
   }
 
-  function calculateMinus(str) {
-    var splitStr = str.replace(/ /g, "").split('-')
+  function calculateMinus (str) {
+    var splitStr = str.replace(/ /g, '').split('-')
     console.log(splitStr)
     var firstNum = parseInt(splitStr[0])
     var secondNum = parseInt(splitStr[1])
-    var result = basic_calc.minus(firstNum,secondNum)
+    var result = basic_calc.minus(firstNum, secondNum)
     displayResult(result)
   }
 
-  function calculateMultiply(str) {
-    var splitStr = str.replace(/ /g, "").split('*')
+  function calculateMultiply (str) {
+    var splitStr = str.replace(/ /g, '').split('*')
     console.log(splitStr)
     var firstNum = parseInt(splitStr[0])
     var secondNum = parseInt(splitStr[1])
-    var result = basic_calc.multiply(firstNum,secondNum)
+    var result = basic_calc.multiply(firstNum, secondNum)
     displayResult(result)
   }
 
-  function calculateDivision(str) {
-    var splitStr = str.replace(/ /g, "").split('/')
+  function calculateDivision (str) {
+    var splitStr = str.replace(/ /g, '').split('/')
     console.log(splitStr)
     var firstNum = parseInt(splitStr[0])
     var secondNum = parseInt(splitStr[1])
-    var result = basic_calc.division(firstNum,secondNum)
+    var result = basic_calc.division(firstNum, secondNum)
     displayResult(result)
   }
 
-  function displayResult(str) {
+  function displayResult (str) {
     display.value = str
   }
 
   // if keyevent is 0-9
   // extend to operators +187, -189, *56, /191
   // enter13
+  // delete8
   function checkKeyCode (keycode) {
     if (keycode >= 48 && keycode <= 57) return true
     if (keycode === 187 || keycode === 189) return true
     if (keycode === 56 || keycode === 191) return true
     if (keycode === 13) return true
+    if (keycode === 8) return true
     else return false
   }
 }
